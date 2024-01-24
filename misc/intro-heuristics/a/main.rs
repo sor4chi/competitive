a3c49b733e19046df3e231bf4bb029813be4955e
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-01-25 04:46:44 +0900

    perf: コンテスト開催期間に関する貪欲

--- misc/intro-heuristics/a/main.rs
+++ misc/intro-heuristics/a/main.rs
@@ -1,18 +1,6 @@
 use proconio::input;
 
-fn get_score(c: &[i32], s: &[Vec<i32>], t: &[usize]) -> i32 {
-    let mut last_day = vec![0; 26];
-    let mut score = 0;
-    for (index, &ti) in t.iter().enumerate() {
-        score += s[index][ti - 1];
-        last_day[ti - 1] = index + 1;
-        for (i, &li) in last_day.iter().enumerate() {
-            score -= c[i] * ((index + 1) - li) as i32;
-        }
-    }
-    score
-}
-
+#[derive(Debug, Clone)]
 struct Solver {
     d: usize,
     c: Vec<i32>,
@@ -25,14 +13,14 @@ impl Solver {
         Solver { d, c, s, t: vec![] }
     }
 
-    fn solve(&mut self) {
+    fn solve(&mut self, k: usize) {
         for _ in 0..self.d {
             let mut score = i32::min_value();
             // find the vest day to use
             let mut best_day = 0;
             for d in 0..26 {
                 self.t.push(d + 1);
-                let new_score = get_score(&self.c, &self.s, &self.t);
+                let new_score = self.get_score(k);
                 if new_score > score {
                     score = new_score;
                     best_day = d;
@@ -48,6 +36,24 @@ impl Solver {
             println!("{}", a);
         }
     }
+
+    fn get_score(&self, k: usize) -> i32 {
+        let mut last_day = vec![0; 26];
+        let mut score = 0;
+        for (index, &ti) in self.t.iter().enumerate() {
+            score += self.s[index][ti - 1];
+            last_day[ti - 1] = index + 1;
+            for (i, &li) in last_day.iter().enumerate() {
+                score -= self.c[i] * ((index + 1) - li) as i32;
+            }
+        }
+        for d in self.t.len()..(self.t.len() + k).min(self.d) {
+            for (i, &li) in last_day.iter().enumerate() {
+                score -= self.c[i] * ((d + 1) - li) as i32;
+            }
+        }
+        score
+    }
 }
 
 fn main() {
@@ -57,7 +63,17 @@ fn main() {
         s: [[i32; 26]; d],
     }
 
-    let mut solver = Solver::new(d, c, s);
-    solver.solve();
-    solver.output();
+    let mut max_score_solver = Solver::new(d, c.clone(), s.clone());
+    let mut max_score = i32::min_value();
+    for k in 0..26 {
+        let mut solver = Solver::new(d, c.clone(), s.clone());
+        solver.solve(k);
+        let score = solver.get_score(k);
+        if score > max_score {
+            max_score = score;
+            max_score_solver = solver;
+        }
+    }
+
+    max_score_solver.output();
 }
