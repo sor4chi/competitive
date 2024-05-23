95dbff6d557395f79c16f6aa9c1f16f50ea8da6b
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-05-24 07:36:51 +0900

    chore: introduce visualizer

---
+++ ahc/033/visualizer/wasm/src/lib.rs
@@ -0,0 +1,65 @@
+mod original_lib;
+use wasm_bindgen::prelude::*;
+
+use crate::original_lib::gen as original_gen;
+use original_lib::{parse_input, parse_output, vis_default, Output};
+
+#[wasm_bindgen]
+pub fn gen(seed: i32) -> String {
+    let input = original_gen(seed as u64);
+    input.to_string()
+}
+
+#[wasm_bindgen(getter_with_clone)]
+pub struct Ret {
+    pub score: i64,
+    pub err: String,
+    pub svg: String,
+}
+
+#[wasm_bindgen]
+pub fn vis(_input: String, _output: String, turn: usize) -> Ret {
+    let input = parse_input(_input.as_str());
+    let output = parse_output(&input, _output.as_str());
+    let (score, err, svg) = match output {
+        Ok(out) => {
+            let out = Output {
+                out: out
+                    .out
+                    .iter()
+                    .map(|x| x.iter().take(turn).copied().collect())
+                    .collect(),
+            };
+            vis_default(&input, &out)
+        }
+        Err(err) => (0, err, String::new()),
+    };
+    if !err.is_empty() {
+        println!("{}", err);
+        println!("Score = {}", 0);
+        Ret { score: 0, err, svg }
+    } else {
+        println!("Score = {}", score);
+        Ret {
+            score,
+            err: "".to_string(),
+            svg,
+        }
+    }
+}
+
+#[wasm_bindgen]
+pub fn get_max_turn(_input: String, _output: String) -> usize {
+    let input = parse_input(_input.as_str());
+    let output = parse_output(&input, _output.as_str());
+    match output {
+        Ok(output) => {
+            let mut max_len = 0;
+            for i in 0..output.out.len() {
+                max_len = max_len.max(output.out[i].len());
+            }
+            max_len
+        }
+        Err(_) => 0,
+    }
+}
