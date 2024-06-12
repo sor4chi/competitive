bf7381b1d7b5a0e75c0f00d879f2fb997a855164
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-06-12 22:02:45 +0900

    feat: int_t8で持って高速化 seed0 = 7632 1:23

--- misc/atcoder12live/a.cpp
+++ misc/atcoder12live/a.cpp
@@ -32,16 +32,20 @@ void println() { cout << '\n'; }
 #define YESNO(a) cout << (a ? "YES" : "NO") << '\n';
 
 struct OP {
-    ll l, r, d;
+    int8_t l, r, d;
 
     void print() {
+        // convert int8_t to int
+        int l = this->l;
+        int r = this->r;
+        int d = this->d;
         println(l, r, d);
     }
 };
 
 struct BeamNode {
     vector<OP> ops;
-    vector<ll> v;
+    vector<int8_t> v;
 };
 
 int main() {
@@ -50,34 +54,34 @@ int main() {
     cout.tie(nullptr);
     cout << fixed << setprecision(15);
 
-    ll n, t;
+    int n, t;
     input(n, t);
 
-    vector<vector<ll>> v(t, vector<ll>(n));
+    vector<vector<int>> v(t, vector<int>(n));
     rep(i, t) rep(j, n) input(v[i][j]);
 
-    ll BEAM_SIZE = 2;
+    int BEAM_SIZE = 5;
     vector<BeamNode> beam;
-    beam.push_back({vector<OP>(), vector<ll>(n, 0)});
+    beam.push_back({vector<OP>(), vector<int8_t>(n, 0)});
 
-    ll SPAN = 5;
-    rep(i, t) {
+    int8_t SPAN = 5;
+    for (int i = 0; i < t; i++) {
         vector<BeamNode> next_beam;
         for (auto node : beam) {
-            vector<ll> evi;
-            for (ll j = 0; j + SPAN < n; j += SPAN) {
+            vector<int8_t> evi;
+            for (int8_t j = 0; j + SPAN < n; j += SPAN) {
                 evi.push_back(j);
             }
 
-            for (ll l = 0; l < evi.size(); l++) {
-                for (ll r = l + 1; r < evi.size(); r++) {
-                    for (ll d = -3; d <= 3; d++) {
-                        vector<ll> next_v = node.v;
-                        for (ll j = evi[l]; j < evi[r]; j++) {
+            for (int8_t l = 0; l < evi.size(); l++) {
+                for (int8_t r = l + 1; r < evi.size(); r++) {
+                    for (int8_t d = -3; d <= 3; d++) {
+                        vector<int8_t> next_v = node.v;
+                        for (int8_t j = evi[l]; j < evi[r]; j++) {
                             next_v[j] += d;
                         }
                         vector<OP> next_ops = node.ops;
-                        next_ops.push_back({evi[l], evi[r] - 1, d});
+                        next_ops.push_back({evi[l], (int8_t)(evi[r] - 1), d});
                         next_beam.push_back({next_ops, next_v});
                     }
                 }
@@ -86,10 +90,10 @@ int main() {
 
         cerr << "next_beam.size() = " << next_beam.size() << endl;
 
-        vector<ll> goal_vt = v[i];
+        vector<int> goal_vt = v[i];
         auto eval = [&](BeamNode node) {
-            ll score = 0;
-            for (ll j = 0; j < n; j++) {
+            int score = 0;
+            for (int j = 0; j < n; j++) {
                 score += abs(node.v[j] - goal_vt[j]);
             }
             return score;
