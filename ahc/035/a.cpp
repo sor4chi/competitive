19acb8677b26e4e454f2f2e28d2b03b76d457ebc
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-07-22 02:42:49 +0900

    feat(upsolve/247,659,087): 最大特徴を優先的に使うようにした

--- ahc/035/a.cpp
+++ ahc/035/a.cpp
@@ -58,30 +58,6 @@ void output_board(vector<vector<int>> board) {
     }
 }
 
-// double randnormal(std::mt19937& generator, normal_distribution<double>& distribution) {
-//     return distribution(generator);
-// }
-
-// normal_distribution<double> distribution(0.0, 1.0);
-// vector<int> generate_vector(int M) {
-//     vector<double> x_prime(M);
-//     double sum_squares = 0.0;
-
-//     for (int i = 0; i < M; ++i) {
-//         x_prime[i] = abs(randnormal(engine, distribution));
-//         sum_squares += x_prime[i] * x_prime[i];
-//     }
-
-//     double pk = 100.0 / sqrt(sum_squares);
-//     vector<int> x(M);
-
-//     for (int i = 0; i < M; ++i) {
-//         x[i] = round(pk * x_prime[i]);
-//     }
-
-//     return x;
-// }
-
 vector<int> generate_next_seed(vector<int> a, vector<int> b) {
     // 各要素ごとに等しい確率で選ぶ
     int m = a.size();
@@ -114,7 +90,6 @@ vector<vector<int>> generate_next_seed_map(vector<vector<int>>& board, vector<ve
                     continue;
                 }
                 vector<int> a = seed_map[board[i][j]];
-                eprintln("ni", ni, "nj", nj, "board[ni][nj]", board[ni][nj]);
                 vector<int> b = seed_map[board[ni][nj]];
                 vector<int> next_seed = generate_next_seed(a, b);
                 next_seeds[{board[i][j], board[ni][nj]}] = next_seed;
@@ -144,39 +119,41 @@ int eval_seed_map(vector<vector<int>> seed_map) {
 vector<vector<int>> generate_next_board_in_greedy(vector<vector<int>> seed_map, int n) {
     vector<vector<int>> next_board(n, vector<int>(n));
 
-    // 一番平均的にいいやつを選ぶ
-    int max_score = 0;
-    int max_seed = 0;
+    // 各特徴の最大値を求める
+    vector<int> max_features(seed_map[0].size());
     for (int i = 0; i < seed_map.size(); i++) {
-        int score = 0;
         for (int j = 0; j < seed_map[i].size(); j++) {
-            score += seed_map[i][j];
-        }
-        if (score > max_score) {
-            max_score = score;
-            max_seed = i;
+            max_features[j] = max(max_features[j], seed_map[i][j]);
         }
     }
+
+    // 最大値を各seedが何個持っているかを求める
+    vector<pair<int, int>> seed_count(seed_map.size());
+    for (int i = 0; i < seed_map.size(); i++) {
+        int count = 0;
+        for (int j = 0; j < seed_map[i].size(); j++) {
+            if (seed_map[i][j] == max_features[j]) {
+                count++;
+            }
+        }
+        seed_count[i] = {count, i};
+    }
+    // 降順にソート
+    sort(seed_count.begin(), seed_count.end(), greater<pair<int, int>>());
 
     // bfsする
     set<int> used;
     vector<vector<bool>> visited(n, vector<bool>(n));
     pair<int, int> start = {n / 2, n / 2};
     queue<pair<pair<int, int>, int>> q;
-    q.push({start, max_seed});
-    used.insert(max_seed);
+    int idx = 0;
+    q.push({start, seed_count[idx].second});
+    used.insert(seed_count[idx].second);
     visited[start.first][start.second] = true;
     while (!q.empty()) {
         auto [p, seed] = q.front();
         q.pop();
         next_board[p.first][p.second] = seed;
-
-        // // seedで特徴量が低いにソートする
-        // vector<pair<int, int>> seed_features;
-        // for (int i = 0; i < seed_map[seed].size(); i++) {
-        //     seed_features.push_back({seed_map[seed][i], i});
-        // }
-        // sort(seed_features.begin(), seed_features.end());
         for (auto [dx, dy] : directions) {
             int ni = p.first + dx;
             int nj = p.second + dy;
@@ -187,41 +164,11 @@ vector<vector<int>> generate_next_board_in_greedy(vector<vector<int>> seed_map, 
                 continue;
             }
             visited[ni][nj] = true;
-            // // seedで特徴量が低い順に5つの合計が最も高いものを選ぶ
-            // int max_score = INT_MIN;
-            // int max_seed = 0;
-            // for (int i = 0; i < seed_map.size(); i++) {
-            //     if (used.count(i)) {
-            //         continue;
-            //     }
-            //     int score = 0;
-            //     for (int j = 0; j < 1; j++) {
-            //         score += seed_features[j].first;
-            //     }
-            //     if (score > max_score) {
-            //         max_score = score;
-            //         max_seed = i;
-            //     }
-            // }
 
-            // seedとの各特徴量の合計が一番大きいものを選ぶ
-            int max_score = INT_MIN;
-            int max_seed = 0;
-            for (int i = 0; i < seed_map.size(); i++) {
-                if (used.count(i)) {
-                    continue;
-                }
-                int score = 0;
-                for (int j = 0; j < seed_map[i].size(); j++) {
-                    score += seed_map[i][j] + seed_map[seed][j];
-                }
-                if (score > max_score) {
-                    max_score = score;
-                    max_seed = i;
-                }
-            }
-            q.push({{ni, nj}, max_seed});
-            used.insert(max_seed);
+            // 最大の特徴量を持っているseedから順に選ぶ
+            idx++;
+            q.push({{ni, nj}, seed_count[idx].second});
+            used.insert(seed_count[idx].second);
         }
     }
 
@@ -241,38 +188,8 @@ int main() {
     rep(i, t_len) rep(j, m) input(x[i][j]);
 
     rep(ti, t) {
-        // モンテカルロ法によって、boardのどこにどのseedを置くかを決定する
         vector<vector<int>> max_board(n, vector<int>(n));
         max_board = generate_next_board_in_greedy(x, n);
-        // float max_score = 0;
-        // chrono::system_clock::time_point start = chrono::system_clock::now();
-        // int tl = 2000 / t + 1;
-        // int iter = 0;
-        // while (chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now() - start).count() < tl) {
-        //     iter++;
-        //     vector<vector<int>> cur_x = x;
-        //     vector<vector<int>> board(n, vector<int>(n));
-        //     vector<int> perm(x.size());
-        //     iota(perm.begin(), perm.end(), 0);
-        //     shuffle(perm.begin(), perm.end(), engine);
-        //     perm.resize(n * n);
-        //     rep(i, n) rep(j, n) {
-        //         board[i][j] = perm[i * n + j];
-        //     }
-        //     cur_x = generate_next_seed_map(board, x);
-        //     // この後、boardにseedを貪欲に置いてnext_seedを生成する操作をt - ti回繰り返す
-        //     // その後、boardのスコアを計算して、max_scoreを超えたら更新する
-        //     rep(i, t - ti) {
-        //         vector<vector<int>> next_board(n, vector<int>(n));
-        //         next_board = generate_next_board_in_greedy(cur_x, n);
-        //         cur_x = generate_next_seed_map(next_board, x);
-        //     }
-        //     if (eval_seed_map(cur_x) > max_score) {
-        //         max_score = eval_seed_map(cur_x);
-        //         max_board = board;
-        //     }
-        // }
-        // eprintln("turn", ti, "iter", iter, "score", max_score);
 
         output_board(max_board);
         rep(i, t_len) rep(j, m) input(x[i][j]);
